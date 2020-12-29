52e38a1ffca356399873505f3db9482da926e30d
vineethku001 <vineethku001@gmail.com>
2020-12-29 18:15:35 +0530

    Update dbsg.tf

--- dbsg.tf
+++ dbsg.tf
@@ -10,24 +10,6 @@ security_groups = ["${aws_security_group.web.id}"]
 }
 
 ingress {
-from_port = 22
-to_port = 22
-protocol = "tcp"
-cidr_blocks = ["${var.vpc_cidr}"]
-}
-egress {
-from_port = 80
-to_port = 80
-protocol = "tcp"
-cidr_blocks = ["0.0.0.0/0"]
-}
-egress {
-from_port = 443
-to_port = 443
-protocol = "tcp"
-cidr_blocks = ["0.0.0.0/0"]
-}
-ingress {
 from_port = -1
 to_port = -1
 protocol = "icmp"
